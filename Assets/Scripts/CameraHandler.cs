82167215a11f4801a29ec7d18d7564f7c4df484e
Hadar Segal <Hadar.Segal@intusurg.com>
2022-02-17 10:08:01 +0200

    capture RGB and depth

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Simulation;
+using UnityEngine.Experimental.Rendering;
+using System.IO;
 
 public class CameraHandler : MonoBehaviour
 {
@@ -9,10 +12,15 @@ public class CameraHandler : MonoBehaviour
     public Vector3 moveOffset = new Vector3(4, 0, 0);
 
     Vector3 startPosition;
-    
+    string baseDir;
+    Camera captureCamera;
+
+
     void Start()
     {
+        baseDir = Manager.Instance.GetDirectoryFor(DataCapturePaths.ScreenCapture);
         startPosition = transform.position;
+        captureCamera = GetComponent<Camera>();
     }
 
     
@@ -27,6 +35,13 @@ public class CameraHandler : MonoBehaviour
 
     private void LateUpdate()
     {
-        
+        if (Time.frameCount < 10 || Time.frameCount > 20)
+            return;
+
+        string colorPath = Path.Combine(baseDir, $"rgb_{Time.frameCount}.png");
+        string depthPath = Path.Combine(baseDir, $"depth_{Time.frameCount}.exr");
+
+        CaptureCamera.CaptureColorAndDepthToFile(captureCamera, GraphicsFormat.B8G8R8A8_SRGB, colorPath, CaptureImageEncoder.ImageFormat.Png,
+            GraphicsFormat.R32_SFloat, depthPath, CaptureImageEncoder.ImageFormat.Exr);
     }
 }
