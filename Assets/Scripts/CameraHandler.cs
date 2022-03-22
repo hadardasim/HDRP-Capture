03ef910ee29f7c22a46d6862f7038e5fa43d83b3
Hadar Segal <Hadar.Segal@intusurg.com>
2022-03-22 10:47:15 +0200

    refactor to use mover script

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -6,12 +6,7 @@ using UnityEngine.Experimental.Rendering;
 using System.IO;
 
 public class CameraHandler : MonoBehaviour
-{
-    // number of frames to move from one side to the other
-    public int moveFrameCounter = 4;
-    public Vector3 moveOffset = new Vector3(4, 0, 0);
-
-    Vector3 startPosition;
+{    
     string baseDir;
     Camera captureCamera;
 
@@ -19,18 +14,14 @@ public class CameraHandler : MonoBehaviour
     void Start()
     {
         baseDir = Manager.Instance.GetDirectoryFor(DataCapturePaths.ScreenCapture);
-        startPosition = transform.position;
-        captureCamera = GetComponent<Camera>();
+        
+        if (captureCamera == null)
+            captureCamera = GetComponent<Camera>();
     }
 
     
     void Update()
-    {
-        int _step = Time.frameCount % (moveFrameCounter * 2);
-        float _ratio = _step / (float)moveFrameCounter;
-        if (_step > moveFrameCounter)
-            _ratio = 2 - _ratio;
-        transform.position = startPosition + moveOffset * _ratio;
+    {        
     }
 
     private void LateUpdate()
